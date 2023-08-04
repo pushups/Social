087f3a80aade15495fc4e6f2009f9618aa3807db
Mike Williams <573678+pushups@users.noreply.github.com>
2023-08-03 21:42:47 -0700

    Initial Commit

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Social.Models;
+
+namespace Social.Controllers;
+
+public class PostsController : Controller
+{
+    private readonly ILogger<PostsController> _logger;
+
+    public PostsController(ILogger<PostsController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Social.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
